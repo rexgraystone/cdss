f2f49bbbd63e5edc9c1e84da8c4e4f24721de35c
Raghav Nanjappan <r6nanjappan@gmail.com>
2023-02-07 11:47:39 +0530

    .

--- 1a_CharCount.c
+++ 1a_CharCount.c
@@ -4,3 +4,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int main() {
+    FILE *filePointer;
+    char ch;
+    int charCount = 0, wordCount = 0, spaceCount = 0, lineCount = 0;
+    filePointer = fopen("1a_CharCount.c", "r");
+    if (filePointer == NULL) {
+        printf("File does not exist.");
+    }
+    else {
+        while ((ch = fgetc(filePointer)) != EOF) {
+            charCount++;
+            if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\0') {
+                spaceCount++;
+            }
+            if (ch == '\n' || ch == '\0') {
+                lineCount++;
+            }
+        }
+        wordCount = spaceCount + 1;
+        printf("Number of characters: %d", charCount);
+        printf("\nNumber of words: %d", wordCount);
+        printf("\nNumber of spaces: %d", spaceCount);
+        printf("\nNumber of lines: %d\n", lineCount);
+    }
+    fclose(filePointer);
+    return 0;
+}
+
+ 
---
+++ 1b_IdentifierCount.c
@@ -0,0 +1,6 @@
+/*
+    1b. Write a C Program to recognize and count the number of identifiers in a file.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
