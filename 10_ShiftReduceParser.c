691b027504a0056d14bf0392dfcf65280cd6d8bc
Raghav Nanjappan <r6nanjappan@gmail.com>
2023-04-11 11:12:45 +0530

    Update programs

---
+++ 10_ShiftReduceParser.c
@@ -0,0 +1,7 @@
+/*
+    10. Write a C Program C Program to implement Shift Reduce Parser for the given grammar
+        E → E+E
+        E → E*E
+        E → (E)
+        E → id
+*/
---
+++ 9_FIRST.c
@@ -0,0 +1,14 @@
+/*
+    9. Write a C Program to find the FIRST in context free grammar.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char gram[10][10], first[10][10], follow[10][10];
+    int i;
+
+    
+    return 0;
+}
